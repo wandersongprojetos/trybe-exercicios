bdf562544089796dc23bb2f1ec4e6e7cce40d1f8
Wanderson Gomes Morais <vandersin5906@gmail.com>
2022-04-07 15:01:36 -0300

    ultimoo exercicio feito

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,89 @@
+//Exercicio 01//
+
+let a=5;
+let b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+
+//Exercicio 02//
+
+if(b > a)
+{
+    console.log("numero B é maior");
+
+}
+
+else
+{
+    console.log("numero A é maior");
+}
+
+//Exercicio 03//
+
+const primeiro =10;
+const segundo =15;
+const terceiro =20;
+
+if (terceiro > primeiro)
+{
+ console.log( "Primeiro é Menor que terceiro")
+
+}
+else if (primeiro < segundo ){
+    console.log( "Primeiro é Menor que segundo")
+}
+
+else {
+    console.log( "Segundo é maior")
+}
+
+//Exercicio 04//
+
+const valor = 5;
+
+switch(valor)
+{
+    case valor >= 10 : 
+        console.log("positive");
+    break;
+
+    case valor < 0 :
+        console.log ("Negativo");
+    break;
+
+    default : console.log("0");
+}
+
+//Exercicio 05//
+const const1 =60;
+const const2 =60;
+const const3 =60;
+const triangulo =180;
+
+
+if(const1+const2+const3 === triangulo )
+{
+    console.log(true);
+}
+else
+{
+    console.log(false);
+}
+
+//Exercicio 06//
+let cavaloxadrex = cavaloxadrex = "Movimento L";
+
+if(cavaloxaxadrex == toLowerCase("Movimento L"))
+{
+ console.log(cavaloxadrex)
+}
+else if(cavaloxadrex == "movimento direita")
+{
+    console.log(cavaloxadrex)
+}
+else{
+    console.log("nenhum movimento")
+}
