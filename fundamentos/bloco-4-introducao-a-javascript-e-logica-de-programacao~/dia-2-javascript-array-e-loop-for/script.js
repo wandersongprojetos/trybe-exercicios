02428e6cfadeaea6c4b3e7c97c492e3f20f1213d
Wanderson Gomes Morais <vandersin5906@gmail.com>
2022-04-07 19:33:34 -0300

    fazendo exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao~/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,60 @@
+//Primeiro exercicio ;//
+
+let numbers1 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]; 
+console.log(numbers1)
+
+//Segundo exercicio//
+
+let numbers2 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma1=0
+for(index=0; index < numbers2.length; index++)
+{
+    soma1+=numbers2[index]
+
+}
+console.log(soma1);
+
+//Terceiro exercicio//
+
+let numbers3 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma2=0
+for(index=0; index < numbers2.length; index++)
+{
+    soma2+=numbers3[index]
+
+}
+let total = soma2 / numbers3.length
+console.log(total);
+
+//Quarto exercicio//
+
+if(total === 27.8 )
+
+{
+    console.log("valor maior que 20");
+}
+else
+{
+    console.log("valor menor ou igual a 20")
+}
+
+//Quinto exercicio//
+
+let numbers5 = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = numero[0];
+for(let index=1; index < numbers5.length; index++)
+{
+      if(  numbers5[index] > maior )
+
+      {
+          maior = numbers5[index];
+      }
+}
+
+console.log(maior)
+
+
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+
